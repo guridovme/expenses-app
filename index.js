cd00bd132ce51ee82f748cd7b6cc405e6dedab72
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-05-16 17:54:21 +0200

    1

---
+++ index.js
@@ -0,0 +1,8 @@
+const expenses=[];
+const inputNode=document.querySelector('.js-input');
+const buttonNode=document.querySelector('.js-button');
+
+buttonNode.addEventListener('click', function() {
+    const expense=inputNode.value;
+    expenses.push(expense);
+});
