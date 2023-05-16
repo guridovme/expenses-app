0604814e15d5ef82e95d192edb2b3214cf1060ff
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-05-16 22:17:47 +0200

    3

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const inputNode=document.querySelector('.js-input');
 const buttonNode=document.querySelector('.js-button');
 
 buttonNode.addEventListener('click', function() {
+    if (inputNode.value==='') {
+        console.log('пустая строка в поле ввода');
+    }
     const expense=parseInt (inputNode.value);
     expenses.push(expense);
+    inputNode.value='';
 });
