fec258bef1a175f1b255c8d460899aa152faf4d5
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-05-17 10:11:25 +0200

    1

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const expenses=[];
+
 const inputNode=document.querySelector('.js-input');
 const buttonNode=document.querySelector('.js-button');
+const historyNode=document.querySelector('.js-history');
+
 
 buttonNode.addEventListener('click', function() {
-    if (inputNode.value==='') {
+    // 1. Получаем значение из поля ввода
+    if (!inputNode.value) {
         return;
     }
+
     const expense=parseInt (inputNode.value);
-    expenses.push(expense);
     inputNode.value='';
+
+    // 2. Сохраняем трату в список
+    expenses.push(expense);
+
+    // 2. Выведем новый список трат
+    
 });
