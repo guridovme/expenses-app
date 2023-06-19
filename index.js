04b845e8e954a9b5188dffd6e5580237df0843dd
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-19 17:06:46 +0200

    2

--- index.js
+++ index.js
@@ -1,54 +1,86 @@
+// Объявление переменных - строковых констант
 const CURRENCY ='руб.';
 const STATUS_IN_LIMIT = 'все хорошо';
 const STATUS_OUT_OF_LIMIT = 'все плохо';
 const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
 
+// Объявление переменных - ссылок на html элементы
 const inputNode=document.getElementById("expenseInput");
+const categorySelectNode=document.getElementById("categorySelect");
 const addButtonNode=document.getElementById("addButton");
 const clearButtonNode = document.getElementById("clearButton");
-const limitNode = document.getElementById("limitValue");
 const totalValueNode = document.getElementById("totalValue");
 const statusNode = document.getElementById("statusText");
 const historyList=document.getElementById("historyList");
 
-let expenses=[];
+// Получает лимит из элемента HTML с id limitValue
+const limitNode = document.getElementById("limitValue");
 const limit = parseInt(limitNode.innerText);
 
-const getTotal = () => {
+// Объявление нашей основной переменной
+// При запуске она содержит пустой массив
+// который мы пополняем по нажатию на кнопку Добавить
+let expenses=[]; 
+
+// ---ФУНКЦИИ------------------------------------- 
+
+// Подсчитывает и возвращает сумму всех трат
+function getTotal() {
     let sum = 0;
-    expenses.forEach((expense) => {
-        sum += expense;
+    expenses.forEach(function (expense) {
+        // пробегаем по массиву объектов expense, берем из каждого поле amount
+        // и прибавляем к пепременной sum
+        sum += expense.amount;
     });
 
     return sum;
 };
 
-const renderStatus = () => {
+// Отрисовывает (обновляет) блок с "Всего", "Лимит" и "Статус"
+function renderStatus() {
+    // создаем переменную total (всего) и записываем в нее результат выполнения getTotal
     const total = getTotal(expenses);
     totalValueNode.innerText = total;
-
+    // условие сравнение - что больше: всего или лимит
     if (total <= limit) {
-        
+        // всего меньше чем лимит - все хорошо
         statusNode.innerText = STATUS_IN_LIMIT;
         statusNode.className = "stats__statusText_positive";
     } else {
+        // всего больше чем лимит - все плохо
+        // шаблонная строка  - строка в которую можно вставить переменные
+        // тут вставлена переменная STATUS_OUT_OF_LIMI
+        // и будет вставлено значение разницы между лимитом и общей суммой расходов
         statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} ${CURRENCY})`;
         statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
     }
 };
 
-const renderHistory = () => {
+// отрисовывает (обновляет) блок
+function renderHistory () {
     historyList.innerHTML = "";
+    // сокращенная запись
+    // expenses.foreach((expense) => {
+
+    // цикл по массиву expenses, где каждый элемент - запись о расходе (сумма и категория)
     expenses.forEach((expense) => {
+    // создание элемента li  (он пока создан только в памяти)
     const historyItem = document.createElement("li");
+
+    // через свойство className также можно прописывать классы
     historyItem.className = "rub";
-    historyItem.innerText = expense;
+
+    // снова создаем шаблонную строку
+    // формата "категория" - "сумма" (а не наоборот, чтобы не усложнять html)
+    historyItem.innerText = `${expense.category} - ${expense.amount}`;
+
+    // берем наш li из памяти и вставляем в документ, в конец historyList
     historyList.appendChild(historyItem);
   });
 };
 
 //Отрисовывает (обновляет) весь интерфейс (в нашем случае - историю, всего, статус)
-const render = () => {
+function render () {
     //вызываем функцию обновления статуса и "всего"
     renderStatus();
     //вызываем функцию обновления истории
@@ -57,7 +89,15 @@ const render = () => {
 
 //Возвращает введенную пользователем сумму
 
-const getExpenseFromUser = () => parseInt (inputNode.value);
+function getExpenseFromUser () {
+    return parseInt (inputNode.value);
+}
+
+// Возвращает выбранную пользователем категорию
+function getSelectedCategory() {
+    return categorySelectNode.value;
+}
+
 
 // функция очистки поля ввода суммы
 // на вход получает переменную input, в которой мы ожидаем html элемент input
@@ -70,7 +110,7 @@ function clearInput(input) {
 */
 
 
-const clearInput = () => {
+const clearInput = function (input) {
     inputNode.value="";
 };
 
